14b515d1374f7a9352fc1410382228410281839e
Pedro <ph@elevenfinancial.com>
2019-12-06 00:10:36 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit and save to reload.</p>
+        <p>Edit and save to reload. learn react</p>
         <a
           className="App-link"
           href="ht
