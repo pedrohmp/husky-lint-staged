b1ca8bd1152f4c1d4d8d5e653a0c5a8a011f2c05
Pedro Porta <ph@elevenfinancial.com>
2019-12-26 13:55:21 -0300

    test

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header
+        className="App-header"
+      >
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit and save to reload. learn react</p>
         <a
+
           className="App-link"
+
+
           href="https://reactjs.org"
-          rel="noopener nore   ferrer"
+          rel="noopener nore ferrer"
         >
           Learn
         </a>
